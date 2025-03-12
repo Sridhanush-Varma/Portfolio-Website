a24de276d532dac91b970e36b7aeb9e0a1bb5fdc
Sridhanush-Varma <sridhanushvarmasv@gmail.com>
2025-03-13 02:01:15 +0530

    initial commit

---
+++ Portfolio_Website/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Header, Skills, Education, Projects, Section } from './components';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Section id="summary">
+          <h2>Professional Summary</h2>
+          <p>A Computer Science Aspirant eager to explore the depths of Python Programming Language and dive deep into concepts like Computer Vision and Image Processing. Experienced in Python, OpenCV, NumPy, NTLK, Speech Recognition and Web Development.</p>
+        </Section>
+        <Skills />
+        <Education />
+        <Projects />
+      </main>
+    </div>
+  );
+}
+
+export default App; 
